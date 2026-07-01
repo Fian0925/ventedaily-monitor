690d49b59ad47a5f4f1dff8c8050b07a267030e5
fian2504 <l200160138@student.ums.ac.id>
2026-07-01 16:08:33 +0700

    Hide API keys and use environment variables

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 # Konfigurasi Telegram Bot
-TELEGRAM_BOT_TOKEN = "8642231143:AAECE4Q90PV9elNwtwMP-zBlsHfw2sm3EDY"
-TELEGRAM_CHAT_ID = "457147602"
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "GANTI_DENGAN_TOKEN_BOT_KAMU")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "457147602")
 
 # Konfigurasi Supabase
-SUPABASE_URL = "https://yjfodwmoouhmajdollku.supabase.co"
-SUPABASE_KEY = "sb_secret_" + "g8xGF_NyGCR8vAM1pJUNgg_51sLF_n_"
+SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://yjfodwmoouhmajdollku.supabase.co")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "GANTI_DENGAN_KUNCI_SUPABASE")
 
 # Konfigurasi Scraping
 BASE_URL = "https://pos.ventedaily.net/product-stock"
